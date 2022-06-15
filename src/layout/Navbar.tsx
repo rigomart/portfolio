2ba48283bde9c7f556b98f61f803b66ca43ae485
Miguel Rodriguez <miguel.rodriguez18@unmsm.edu.pe>
2022-06-14 19:32:12 -0500

    feat: hide/show navbar on scroll

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -2,10 +2,17 @@ import Logo from "@/logo.svg?component";
 import { sectionLinks } from "@utils/constants";
 import Drawer from "./Drawer";
 import SectionLink from "./SectionLink";
+import useShowOnScroll from "../hooks/useShowOnScroll";
 
 const Navbar = () => {
+  const { show } = useShowOnScroll();
+
   return (
-    <header className='fixed w-full px-4 sm:px-14 z-50 bg-ocean-900 flex items-center justify-between h-20'>
+    <header
+      className={`${
+        show ? "" : "-translate-y-20"
+      } fixed transition duration-150 ease-in-out w-full px-4 sm:px-14 z-50 bg-ocean-900 flex items-center justify-between h-20`}
+    >
       <Logo />
       <nav>
         <ul className='hidden sm:flex gap-x-10'>
