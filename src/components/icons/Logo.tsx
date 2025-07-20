6a0a26c920e368db85725ed44870d2dfd1ee61c4
Miguel Rodriguez <mirdor.dev@gmail.com>
2025-07-20 00:08:39 -0500

    clean project

--- src/components/icons/Logo.tsx
+++
@@ -1,7 +0,0 @@
-import LogoIcon from "@/logo.svg?component";
-
-const Logo = () => {
-  return <LogoIcon className='h-24' />;
-};
-
-export default Logo;
--- src/hooks/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as useClickAway } from "./useClickAway";
-export { default as useShowOnScroll } from "./useShowOnScroll";
-export { default as useFeaturedProjects } from "./useFeaturedProjects";
-export { default as useProjects } from "./useProjects";
-export { default as useMounted } from "./useMounted";
